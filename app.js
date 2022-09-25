f7a4140d6af267a113e7431c039602e85b831684
Ayush Kumar Bhadani <ayushkumarbhadani@gmail.com>
2022-09-25 16:04:27 +0530

    set up express

---
+++ app.js
@@ -0,0 +1,16 @@
+const express=require("express");
+const app=express();
+
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.json(({
+        "TEST":"Working"
+    }));
+});
+
+const PORT=8000 || process.env
+app.listen(PORT,()=>{
+    console.log(`App is listning on port ${PORT}`);
+})
